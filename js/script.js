2bddf4b448513a9330e41730bb76da58203cb5dc
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-07-11 15:34:23 +0800

    complete sidebar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// const dashboardIconStates = {
+//     'default': "./imgs/note.png",
+//     'hover': "./imgs/note.gif"
+// };
+
+// let dashboardIconImg = document.querySelector("#dashboard-icon");
+
+// dashboardIconImg.addEventListener('mouseenter', function(e) {
+//     dashboardIconImg.setAttribute('src', dashboardIconStates.hover);
+// });
+
+// dashboardIconImg.addEventListener('mouseleave', function(e) {
+//     dashboardIconImg.setAttribute('src', dashboardIconStates.default);
+// });
